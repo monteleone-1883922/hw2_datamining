8bc1f04d7ad5fc235c8b36bc42a4814bd9fdd66f
AleG19 <monteleone.1883922@studenti.uniroma1.it>
2023-11-04 11:21:22 +0100

    added typing, DataInfo, handling product errors

--- DM_Homework_2__1883922_Alessandro_Monteleone/amazon_products_retrieval.py
+++ DM_Homework_2__1883922_Alessandro_Monteleone/amazon_products_retrieval.py
@@ -6,49 +6,55 @@ from fake_useragent import UserAgent
 from utils_and_classes import *
 import time
 import warnings
+from typing import *
 
 
 
 
-def build_url(keyword,page):
+def build_url(keyword : str,page : str) -> str:
     return URL.format(keyword,page)
 
 
-def get_page(url, headers):
+def get_page(url : str, headers : Dict[str:str]) -> r.Response:
 
     response = r.get(url, headers=headers)
     handle_status_codes(response)
     return response
 
-def set_user_agent(user_agent):
+def set_user_agent(user_agent : str) -> Dict[str:str]:
     headers = HEADERS
     headers["User-Agent"] = user_agent    
     return headers
 
-def set_random_user_agent(ua):
+def set_random_user_agent(ua : UserAgent) -> Dict[str:str]:
     return set_user_agent(ua.random)
 
 
 
-
+#ASUS TUF Gaming NVIDIA GeForice RTX 3070 V2 OC Edition Scheda Grafica, 8GB GDDR6, PCIe 4.0, HDMI 2.1, DisplayPort 1.4a, GPU Tweak II, Componenti di Livello Militare, LHR
 def test():
    pass
         
 
 def store_amazon_products():
+    
     warnings.filterwarnings('ignore', category=UserWarning, module='fake_useragent')
-    ua = UserAgent()
+    # ua = UserAgent()
+    headers = HEADERS #set_random_user_agent(ua)
+    data_info = DataInfo()
     with open("DM_Homework_2__1883922_Alessandro_Monteleone/amazon_products_gpu.tsv","w") as file:
         file.write("description\tprice\tprime\turl\tstars\tnum_reviews\n")
         for i in range(MAX_NUM_PAGES):
             print("getting page ", i+1)
             url = build_url(KEYWORD,i+1)
-            headers = set_random_user_agent(ua)
+            
             page = get_page(url, headers)
             tree = etree.HTML(page.text)
+            with open("test.html","w") as f:
+                f.write(lxml.etree.tostring(tree, pretty_print=True, encoding='unicode'))
             products = html_find(tree,"div",PRODUCTS_CLASS) + html_find(tree,"div",PRODUCTS_AD_CLASS)
             for product_html in products:
-                product = Product(product_html)
+                product = Product(product_html,data_info)
                 file.write(product.to_string_tsv() + "\n")
             time.sleep(20)
     warnings.filterwarnings('default', category=UserWarning, module='fake_useragent')
--- DM_Homework_2__1883922_Alessandro_Monteleone/utils_and_classes.py
+++ DM_Homework_2__1883922_Alessandro_Monteleone/utils_and_classes.py
@@ -1,12 +1,14 @@
 
 import lxml
+from typing import *
+from requests import Response
 
 URL =  "https://www.amazon.it/s?k={}&page={}"
 KEYWORD = "gpu"
 MAX_NUM_PAGES = 10
 HEADERS = {
         "Host": "www.amazon.it",
-        "User-Agent": "{}",       #"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",#"{}",       #"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3",
@@ -27,14 +29,14 @@ PRODUCT_PAGE_LINK = "a-link-normal s-underline-text s-underline-link-text s-link
 PRODUCT_STARS = "a-icon-alt"
 PRODUCT_REVIEWS = "a-size-base s-underline-text"
 
-def html_find(html_fragment, element, html_class = ""):
+def html_find(html_fragment, element : str, html_class : str = ""):
      if html_class == "":
         return html_fragment.xpath(f'.//{element}') 
      else :
          return html_fragment.xpath(f'.//{element}[@class="{html_class}"]') 
 
 
-def handle_status_codes(response):
+def handle_status_codes(response : Response) -> None:
     if response.status_code == 200:
         print("The request was successfully processed (HTTP 200 OK)")
     elif response.status_code == 204:
@@ -63,16 +65,71 @@ def handle_status_codes(response):
         exit(1)
 
 
+class DataInfo():
+
+    def __init__(self):
+        self.description_errors = 0
+        self.prime_errors = 0
+        self.price_errors = 0
+        self.stars_errors = 0
+        self.num_reviews_errors = 0
+        self.url_errors = 0
+        self.num_products = 0
+
+    def __str__(self) -> str:
+        return "total number of products = " + str(self.num_products) + \
+            "\nERRORS\n" + \
+            "description errors = " + str(self.description_errors) + \
+            "prime errors = " + str(self.prime_errors) + \
+            "price errors = " + str(self.price_errors) + \
+            "stars errors = " + str(self.stars_errors) + \
+            "number of reviews errors = " + str(self.num_reviews_errors) + \
+            "url errors = " + str(self.url_errors)
+    
+
+
+
 class Product():
     
-    def __init__(self,html_product):
-        self.description = html_find(html_product, "span",PRODUCT_DESCRIPTION_CLASS )[0].text
-        self.price = html_find(html_product, "span",PRODUCT_PRICE )[0].text[:-2]
-        self.prime = len(html_find(html_product,"i",PRIME)) != 0
-        self.url = "https://www.amazon.it" + html_find(html_product,"a",PRODUCT_PAGE_LINK)[0].get("href")
-        self.stars = html_find(html_product, "span", PRODUCT_STARS)[0].text[:3]
-        self.num_reviews = html_find(html_product, "span", PRODUCT_REVIEWS)[0].text
+    def __init__(self,html_product, data_info : DataInfo):
 
-    def to_string_tsv(self):
+        data_info.num_products += 1
+        
+        self.description = self.get_field_handle_errors("description", html_product, "span",PRODUCT_DESCRIPTION_CLASS )
+        self.price = self.get_field_handle_errors("price",html_product, "span",PRODUCT_PRICE )
+        self.prime = self.get_field_handle_errors("prime",html_product,"i",PRIME)
+        self.url = self.get_field_handle_errors("url",html_product,"a",PRODUCT_PAGE_LINK)
+        self.stars = self.get_field_handle_errors("stars",html_product, "span", PRODUCT_STARS)
+        self.num_reviews = self.get_field_handle_errors("num_reviews",html_product, "span", PRODUCT_REVIEWS)
+    
+
+    def to_string_tsv(self) -> str:
         return self.description + "\t" + self.price + "\t" + str(self.prime) + "\t" + self.url + "\t" + self.stars + "\t" + self.num_reviews
         
+    def get_field_handle_errors(self,field : str,html_product,element : str, html_class : str, data_info : DataInfo) -> str:
+        try:
+            if field == "description" or field == "num_reviews":
+                result = html_find(html_product,element, html_class)[0].text
+            elif field == "price":
+                result = html_find(html_product, "span",PRODUCT_PRICE )[0].text[:-2]
+            elif field == "prime":
+                result = len(html_find(html_product,"i",PRIME)) != 0
+            elif field == "url":
+                result = "https://www.amazon.it" + html_find(html_product,"a",PRODUCT_PAGE_LINK)[0].get("href")
+            elif field == "stars":
+                result = html_find(html_product, "span", PRODUCT_STARS)[0].text[:3]
+        except:
+            result = ""
+            if field == "description":
+                data_info.description_errors += 1
+            elif field == "price":
+                data_info.price_errors += 1
+            elif field == "prime":
+                data_info.prime_errors += 1
+            elif field == "url":
+                data_info.url_errors += 1
+            elif field == "stars":
+                data_info.stars_errors += 1
+            elif field == "num_reviews":
+                data_info.num_reviews_errors += 1
+        return result
