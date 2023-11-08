61b19b72d2161fe8a55554054c258b37fa04715a
AleG19 <monteleone.1883922@studenti.uniroma1.it>
2023-11-08 21:58:15 +0100

    added top10 ratings, weighted ratings, cos similarity

--- DM_Homework_2__1883922_Alessandro_Monteleone/server/amazon_product_analysis.py
+++ DM_Homework_2__1883922_Alessandro_Monteleone/server/amazon_product_analysis.py
@@ -1,6 +1,7 @@
 import json
 
 import pandas as pd
+from math import log2
 from typing import *
 from utils_and_classes import SentencePreprocessing, STOPWORDS_FILE_PATH, SPECIAL_CHARACTERS_FILE_PATH, INDEX_FILE_PATH, \
     CATEGORIES
@@ -11,11 +12,11 @@ def load_data() -> pd.DataFrame:
     return pd.read_csv("../data/amazon_products_gpu.tsv", sep="\t")
 
 
-def top10(df: pd.DataFrame, column: str, numeric: bool = True, higher: bool = True) -> pd.DataFrame:
+def topN(df: pd.DataFrame, column: str, n: int, numeric: bool = True, higher: bool = True) -> pd.DataFrame:
     if numeric:
         df[column] = df[column].apply(lambda x: x.replace(".", "").replace(",", ".") if pd.notna(x) else x)
         df[column] = df[column].astype(float)
-    return df.sort_values(by=column, ascending=not higher).head(10)
+    return df.sort_values(by=column, ascending=not higher).head(n)
 
 
 def build_heap_cos_similarity(query, inverted_index, documents_normas):
@@ -27,9 +28,16 @@ def build_heap_cos_similarity(query, inverted_index, documents_normas):
         for document in documents_containing_word:
             documents_similarities[document[0]] = documents_similarities.get(document[0], 0) + document[1] / \
                                                   documents_normas[document[0]][1]
-    for document in documents_similarities:
-        h.heappush(cos_similarity_heap, documents_similarities[document])
+    for document in documents_similarities.items():
+        h.heappush(cos_similarity_heap, (-document[1], document[0]))
     return cos_similarity_heap
+
+def get_query_result(df,heap,n=-1):
+    result_of_query = []
+    num_results = len(heap) if n == -1 or n > len(heap) else n
+    for _ in range(num_results):
+        result_of_query.append(h.heappop(heap)[1])
+    return df.loc[result_of_query]
 
 
 def price_range_for_categories(description_and_price, categories):
@@ -43,8 +51,8 @@ def price_range_for_categories(description_and_price, categories):
         for category in categories:
             if category in description:
                 categories_price_range[category][0] = price if not pd.isna(price) and (
-                            categories_price_range[category][0] == -1 or categories_price_range[category][
-                        0] > price) else categories_price_range[category][0]
+                        categories_price_range[category][0] == -1 or categories_price_range[category][
+                    0] > price) else categories_price_range[category][0]
                 categories_price_range[category][1] = max(categories_price_range[category][1], price)
                 break
     categories_price_range_clean = {
@@ -53,7 +61,7 @@ def price_range_for_categories(description_and_price, categories):
                       categories_price_range[category][0] != -1],
         "max price": [categories_price_range[category][1] for category in categories if
                       categories_price_range[category][0] != -1]
-        }
+    }
     return pd.DataFrame(categories_price_range_clean)
 
 
@@ -63,8 +71,12 @@ def retrieve_index(index_path):
     return data["inverted_index"], data["documents_TFIDF"]
 
 
-def compute_weigthed_ratings(pd):
-    pass
+def compute_weighted_ratings(df):
+    weighted_ratings = [float(row["stars"].replace(",", ".")) * log2(int(row["num_reviews"]))
+                        if pd.notna(row["stars"]) and pd.notna(row["num_reviews"])
+                        else row["stars"] for index, row in df.iterrows()]
+    df["weighted_ratings"] = pd.Series(weighted_ratings)
+
 
 class QueryProcessor():
 
--- DM_Homework_2__1883922_Alessandro_Monteleone/server/app.py
+++ DM_Homework_2__1883922_Alessandro_Monteleone/server/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
 from markupsafe import Markup
-
+from utils_and_classes import SentencePreprocessing, SPECIAL_CHARACTERS_FILE_PATH, STOPWORDS_FILE_PATH
 from amazon_product_analysis import *
+from nltk.tokenize import word_tokenize
 
 app = Flask(__name__)
 df = load_data()
+sp = SentencePreprocessing(STOPWORDS_FILE_PATH, SPECIAL_CHARACTERS_FILE_PATH)
+
 
 @app.route('/')
 def indxe():
@@ -17,29 +20,51 @@ def query_menu():
     table = Markup(table)
     return render_template('base.html', table=table)
 
-@app.route('/top10price' , methods=['get'])
+
+@app.route('/top10price', methods=['get'])
 def top10_prices():
-    table = top10(df,"price").to_html()
+    table = topN(df, "price", 10).to_html()
     table = Markup(table)
     return render_template('fixed_queries.html', table=table)
 
-@app.route('/price_categories' , methods=['get'])
-def price_categories():
-    table = price_range_for_categories(df[["description","price"]],CATEGORIES).to_html()
-    table =  Markup(table)
+
+@app.route('/top10rated', methods=['get'])
+def top10_reviews():
+    table = topN(df, "stars", 10).to_html()
+    table = Markup(table)
     return render_template('fixed_queries.html', table=table)
-@app.route('/fixed_queries' , methods=['get'])
+
+
+@app.route('/top10rated_weighted', methods=['get'])
+def top10_reviews_weighted():
+    compute_weighted_ratings(df)
+    table = topN(df, "weighted_ratings", 10, False).to_html()
+    table = Markup(table)
+    return render_template('fixed_queries.html', table=table)
+
+
+@app.route('/price_categories', methods=['get'])
+def price_categories():
+    table = price_range_for_categories(df[["description", "price"]], CATEGORIES).to_html()
+    table = Markup(table)
+    return render_template('fixed_queries.html', table=table)
+
+
+@app.route('/fixed_queries', methods=['get'])
 def fixed_queries():
     return render_template('fixed_queries_menu.html')
 
 
-@app.route('/query', methods=['POST'])
+@app.route('/query', methods=['post'])
 def process_query():
     user_input = request.form['user_input']
-    result = user_input.lower()
-
-    return render_template('result.html', result=result)
-
+    text_query_raw = user_input.lower()
+    query = sp.preprocess(text_query_raw)
+    inv_indx, norm_indx = retrieve_index(INDEX_FILE_PATH)
+    heap = build_heap_cos_similarity(query, inv_indx, norm_indx)
+    table = get_query_result(df, heap, 10).to_html()
+    table = Markup(table)
+    return render_template('base.html', table=table)
 
 
 if __name__ == '__main__':
