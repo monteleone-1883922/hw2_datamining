44492147aa64874a01d6f7902c5ef2ec81730071
AleG19 <monteleone.1883922@studenti.uniroma1.it>
2023-11-11 13:21:01 +0100

    fixed hashing techniques and added a report txt file

--- DM_Homework_2__1883922_Alessandro_Monteleone/server/hashing_techniques.py
+++ DM_Homework_2__1883922_Alessandro_Monteleone/server/hashing_techniques.py
@@ -4,10 +4,12 @@ from typing import Callable
 import time
 import datetime
 
+from amazon_product_analysis import load_data
+
 
 class Shingling():
 
-    def __int__(self, hash_function, k: int):
+    def __init__(self, hash_function, k: int):
         self.hash = hash_function
         self.k = k
 
@@ -64,7 +66,7 @@ class LSH():
                 bucket = self.apply_hash(fragment, i)
                 buckets[bucket] = buckets.get(bucket, []) + [j]
                 for k in range(len(buckets[bucket]) - 1):
-                    near_elements.add((buckets[bucket][k], j))
+                    near_elements.add((buckets[bucket][k], j) if buckets[bucket][k] < j else (j, buckets[bucket][k]))
         return near_elements
 
     def apply_hash(self, el, i: int):
@@ -81,15 +83,13 @@ class CompareWithLSH():
         self.execution_times = []
 
     def compute_nearest_documents(self, documents):
-        dt = datetime.datetime.now()
-        start_time = int(dt.microsecond / 1000)
+        start_time = int(time.time() * 1000)
         signatures = []
         for document in documents:
             document_shingling = self.shingling.get_shingling(document)
             signatures.append(self.minwise_hashing.get_minwise_hashing(document_shingling))
         result = self.lsh.get_LSH(signatures)
-        dt = datetime.datetime.now()
-        self.execution_times.append(int(dt.microsecond / 1000) - start_time)
+        self.execution_times.append(int(time.time() * 1000) - start_time)
         return result
 
     def get_last_execution_time(self):
@@ -103,20 +103,18 @@ class CompareWithJaccardSimilarity():
         self.execution_times = []
 
     def compute_nearest_documents(self, documents, threshold: float):
-        dt = datetime.datetime.now()
-        start_time = int(dt.microsecond / 1000)
+        start_time = int(time.time() * 1000)
         near_documents = set()
         shinglings = []
         for document in documents:
             shinglings.append(set(self.shingling.get_shingling(document)))
         for i in range(len(shinglings) - 1):
             for j in range(i + 1, len(shinglings)):
-                jaccard_similarity = (len(shinglings[i].union(shinglings[j])) /
-                                      len(shinglings[i].intersection(shinglings[j])))
+                jaccard_similarity = (len(shinglings[i].intersection(shinglings[j])) /
+                                      len(shinglings[i].union(shinglings[j])))
                 if jaccard_similarity >= threshold:
                     near_documents.add((i, j))
-        dt = datetime.datetime.now()
-        self.execution_times.append(int(dt.microsecond / 1000) - start_time)
+        self.execution_times.append(int(time.time() * 1000) - start_time)
         return near_documents
 
     def get_last_execution_time(self):
@@ -131,14 +129,21 @@ def compare_nearest_documents(df, jaccard_similarity: CompareWithJaccardSimilari
     return lsh_nearest, jaccard_nearest
 
 
+
+
 def hashFamily(i):
     resultSize = 8
     # how many bytes we want back
     maxLen = 20
     # how long can our i be (in decimal)
     salt = str(i).zfill(maxLen)[-maxLen:]
+    salt = salt.encode('utf-8')
 
     def hashMember(x):
+        if type(x) is str:
+            x = x.encode('utf-8')
+        elif type(x) is list:
+            x = b"".join(x)
         return hashlib.sha1(x + salt).digest()[-resultSize:]
 
     return hashMember
@@ -152,8 +157,10 @@ class Report():
         self.lsh_num_duplicates = None
         self.jaccard_num_duplicates = None
         self.size_intersection_results = None
+        self.lsh_last_execution_time = None
+        self.jaccard_last_execution_time = None
 
-    def build_report(self,df):
+    def build_report(self, df):
         documents = df["description"].tolist()
         shingling_hash = hashFamily(1)
         minwise_hashes = [hashFamily(i) for i in range(2, 72)]
@@ -164,10 +171,29 @@ class Report():
         lsh_comparator = CompareWithLSH(shingling, minwise_hashing, lsh)
         jaccard_comparator = CompareWithJaccardSimilarity(shingling)
         lsh_result = lsh_comparator.compute_nearest_documents(documents)
-        jaccard_result = jaccard_comparator.compute_nearest_documents(documents)
+        jaccard_result = jaccard_comparator.compute_nearest_documents(documents,0.8)
         self.lsh_result = lsh_result
         self.jaccard_result = jaccard_result
         self.lsh_num_duplicates = len(lsh_result)
         self.jaccard_num_duplicates = len(jaccard_result)
         self.size_intersection_results = len(lsh_result.intersection(jaccard_result))
+        self.lsh_last_execution_time = lsh_comparator.get_last_execution_time()
+        self.jaccard_last_execution_time = jaccard_comparator.get_last_execution_time()
 
+
+def test():
+    df = load_data()
+    report = Report()
+    report.build_report(df)
+    with open("report.txt", "w") as f:
+        f.write("lsh_result: " + str(report.lsh_result) + "\n")
+        f.write("jaccard_result: " + str(report.jaccard_result) + "\n")
+        f.write("lsh_num_duplicates: " + str(report.lsh_num_duplicates) + "\n")
+        f.write("jaccard_num_duplicates: " + str(report.jaccard_num_duplicates) + "\n")
+        f.write("size_intersection_results: " + str(report.size_intersection_results) + "\n")
+        f.write("lsh_last_execution_time: " + str(report.lsh_last_execution_time) + "\n")
+        f.write("jaccard_last_execution_time: " + str(report.jaccard_last_execution_time) + "\n")
+
+
+if __name__ == "__main__":
+    test()
